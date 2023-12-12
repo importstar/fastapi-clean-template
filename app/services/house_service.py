6ea0e1ba4330264a40beeb619928938b36e24293
suraw00t <surawut.kong@gmail.com>
2023-12-12 20:51:39 +0700

    refactor: 💡 update house service

--- app/services/house_service.py
+++ app/services/house_service.py
@@ -4,5 +4,5 @@ from app.services.base_service import BaseService
 
 class HouseService(BaseService):
     def __init__(self):
-        self.house_repository = HouseRepository()
-        super().__init__(self.house_repository)
+        house_repository = HouseRepository()
+        super().__init__(house_repository)
