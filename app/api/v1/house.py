addec6f9b69b90e04776ac19384a9b9e4923741f
suraw00t <surawut.kong@gmail.com>
2023-12-12 20:48:22 +0700

    refactor: 💡 add find house schema to api router

--- app/api/v1/house.py
+++ app/api/v1/house.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter, Depends
 from app import models
 from app.services import HouseService
-from app.schemas.house_schema import ResponseHouse, BaseHouse
-from typing import List, Optional
+from app.schemas.house_schema import ResponseHouse, BaseHouse, FindHouse
+from typing import List, Optional, Annotated
 from fastapi_pagination import Page
 from fastapi_pagination.ext.mongoengine import paginate
 
@@ -10,7 +10,10 @@ router = APIRouter(tags=["house"], prefix="/house")
 
 
 @router.get("/", response_model=Page[ResponseHouse])
-async def house(house_service: HouseService = Depends(HouseService)):
+async def house(
+    house_service: HouseService = Depends(HouseService),
+    find_house: FindHouse = Depends(),
+):
     houses = house_service.get_list()
     return paginate(houses)
 
@@ -19,7 +22,7 @@ async def house(house_service: HouseService = Depends(HouseService)):
 async def create_house(
     house: BaseHouse, house_service: HouseService = Depends(HouseService)
 ):
-    house = house_service.add(house)
+    house = house_service.create(house)
     return house
 
 
